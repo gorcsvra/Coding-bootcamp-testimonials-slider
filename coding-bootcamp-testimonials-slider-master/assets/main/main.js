c980e2d75e14c0cab1b1ac39a9d2dba7c010b65d
gorcsvra <66862598+gorcsvra@users.noreply.github.com>
2021-11-06 14:17:54 +0100

    Kész múködik s karusel

---
+++ coding-bootcamp-testimonials-slider-master/assets/main/main.js
@@ -0,0 +1,73 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//swipe touch
+const photo = document.querySelector(".photo-wrap");
+let isDown = false;
+let startX;
+let scrollLeft;
+let walk;
+console.log(photo);
+
+photo.addEventListener('touchstart', (e) => {
+  isDown = true;
+  startX = e.touches[0].screenX;
+  console.log('start', e.touches[0].screenX);
+}); 
+
+photo.addEventListener("touchend", () => {
+  isDown = false;
+  console.log("walk", walk);
+  if (walk && walk > 0) {
+    plusSlides(-1);
+  } else if (walk && walk < 0) {
+    plusSlides(1);
+  }
+});
+
+photo.addEventListener("touchmove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.touches[0].screenX;
+  walk = x - startX;
+  console.log("move", e.touches[0].screenX);
+});
+
+const next = document.querySelector(".next");
+console.log(next);
+next.addEventListener("click", () => {
+  plusSlides(1);
+});
+
+const prev = document.querySelector(".prev");
+console.log(prev);
+prev.addEventListener("click", function () {
+  plusSlides(-1);
+});
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("photo");
+  let testimonials = document.getElementsByClassName("testimonial");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    testimonials[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  testimonials[slideIndex - 1].style.display = "block";}
